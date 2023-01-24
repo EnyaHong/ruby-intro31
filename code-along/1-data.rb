4b09e91f7735f27c8c0334285797d9b0ed586b96
EnyaHong <enya.hong@kellogg.northwestern.edu>
2023-01-24 22:34:09 +0000

    in class practice

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,48 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 2
+
+puts 5 + 2 
+puts 5 - 2 
+puts 5 * 2
+puts 5 / 2
+puts 5 % 2
+puts 5.0 / 2.0
 
 # Perform simple math with numbers
 
+puts 5 * 2 + 1 
+
 # Strings
+
+puts "Hello, world!"
 
 # Combine strings together
 
+puts "Tacos are " + "delish"
+puts "Tacos" * 3
+#puts 3 * "Tacos" <-- this doesn't work (Ruby doesn't understand)
+puts "Tacos: " + 3.to_s
+
 # Variables
 
+food = "Tacos"
+quantity = 3
+puts food * quantity
+
 # Combine strings and variables
+first_name = "Grogu"
+puts "Hello, " + first_name + "!"
+puts "Hello, #{first_name}!"
+
+greeting = "Hello, #{first_name}!"
+puts greeting
+
+puts "#{food}: #{quantity}"
 
 # String manipulation
+puts first_name.upcase 
+puts greeting.reverse
+puts greeting.swapcase
